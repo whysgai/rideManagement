21100b20d0ae440150bbef22d21413012ce9823e
Will Cohen <will.r.cohen@gmail.com>
2016-12-11 16:24:33 -0500

    Buttons are fully functioning, styling is complete

--- js/main.js
+++ js/main.js
@@ -15,28 +15,45 @@ function addRide(event){
     alert("Form incomplete. Please resubmit");
   }
   else {
+    $("#rideLast").empty();
     var newRide = new Ride($("#rideName").val(), $("#rideColor").val(), $("#ridePpl").val(), $("#rideLvl").val());
     rideList.push(newRide);
+    displayRide(rideList[rideList.length-1],$("#rideLast"))
     $("#rideName, #rideColor, #ridePpl, #rideLvl").val("");
   }
 }
 
 //displayRide takes in value of array index
-function displayRide(ride){
-  var displayString = ride.name+" - Color: "+ride.color+" No. Riders: "+ride.riders+" Ride Level: "+ride.level;
-  $("#rideList").append("<li>"+displayString+"</li>");
+function displayRide(ride, location){
+  var displayString = ride.name+" - Color: "+ride.color+", No. Riders: "+ride.riders+", Ride Level: "+ride.level;
+  location.append("<li>"+displayString+"</li>");
 }
 
 function showRides(event){
   event.preventDefault();
   $("#rideList").empty();
   $.each(rideList, function(index, value){
-    displayRide(value);
+    displayRide(value, $("#rideList"));
   });
+}
+
+function removeRide(event){
+  event.preventDefault();
+  $("#rideLast").empty();
+  rideList.splice(-1,1);
+}
+
+function clearRides(event){
+  event.preventDefault();
+  $("#rideLast").empty();
+  $("#rideList").empty().append("<li>No rides are currently operating.</li>");
+  rideList = [];
 }
 
 
 $(document).ready(function(){
   $("#rideAdd").on("click", function(event){addRide(event)});
   $("#rideShow").on("click", function(event){showRides(event)});
+  $("#rideRemove").on("click", function(event){removeRide(event)});
+  $("#rideClear").on("click", function(event){clearRides(event)});
 });
